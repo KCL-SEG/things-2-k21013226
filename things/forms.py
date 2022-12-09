b41682e6d0ae8b28e3a3d8c21ceb8636f3b092d3
jasdeep panum <jasdeep.panum@kcl.ac.uk>
2022-12-09 10:34:23 +0000

    add max length to description in form.py

--- things/forms.py
+++ things/forms.py
@@ -9,5 +9,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ThingForm(forms.Form):
     name = forms.CharField(label = "Enter Name",max_length=35)
-    description = forms.CharField(widget=forms.Textarea)
+    description = forms.CharField(widget=forms.Textarea[maxlength="120"])
     quantity =  forms.IntegerField(label = "Enter Quantity",validators=[MinValueValidator(0),MaxValueValidator(50)])
+
+#with_asserts.context_manager.SelectorNotFound: No selector matches found for textarea[maxlength="120"]
+#AssertionError: Could not find an input matching the specifications of the description field.
--- things/tests.py
+++ things/tests.py
@@ -1,13 +1,7 @@
-#from django.test import TestCase
-
-# Create your tests here.
 from django import forms
 from with_asserts.case import TestCase
 from django.urls import reverse
 from things.forms import ThingForm
-
-from django.test import TestCase
-
 
 
 class HomeViewTestCase(TestCase):
