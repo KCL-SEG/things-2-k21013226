87de1a984a5effa6ac3cdce07037c34cc6287324
jasdeep panum <jasdeep.panum@kcl.ac.uk>
2022-12-09 10:49:32 +0000

    import model into forms.py

--- things/forms.py
+++ things/forms.py
@@ -2,15 +2,27 @@
 
 # Create your forms here.
 
-
+from .models import Thing
 from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class ThingForm(forms.Form):
-    name = forms.CharField(label = "Enter Name",max_length=35)
-    description = forms.CharField(widget=forms.Textarea[max_length =="120"])
-    quantity =  forms.IntegerField(label = "Enter Quantity",validators=[MinValueValidator(0),MaxValueValidator(50)])
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ["name","description","quantity"]
+        widgets = {'description':forms.Textarea() }
+
+
+
+
+
+
+
+
+    #name = forms.CharField(label = "Enter Name",max_length=35)
+    #description = forms.CharField(widget=forms.Textarea[max_length =="120"])
+    #quantity =  forms.IntegerField(label = "Enter Quantity",validators=[MinValueValidator(0),MaxValueValidator(50)])
 
 #with_asserts.context_manager.SelectorNotFound: No selector matches found for textarea[maxlength="120"]
 #AssertionError: Could not find an input matching the specifications of the description field.
